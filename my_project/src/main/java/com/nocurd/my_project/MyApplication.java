6edf720687adaa9017bd4470907e989b06d16a7e
malan <malan@baiwang.com>
2020-01-10 11:24:02 +0800

    完善下

--- my_project/src/main/java/com/nocurd/my_project/MyApplication.java
+++ my_project/src/main/java/com/nocurd/my_project/MyApplication.java
@@ -1,9 +1,16 @@
 package com.nocurd.my_project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: malan
  * @Description:
  * @Date: Create in 11:12 2020/1/10
  */
+@SpringBootApplication
 public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
 }
